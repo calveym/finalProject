bbfec9413a13806add0cddebdd04da3ab2d2b299
LMCMLJ <mjcalvey2@gmail.com>
2017-11-27 09:46:38 -0500

    Setting up testing framework

---
+++ Test.java
@@ -0,0 +1,29 @@
+public class Test {
+
+    public static void main(String[] args) {
+        setup();
+        // call testing functions here
+
+        // ie
+        test1();
+    }
+
+
+    // set up game env
+
+    public static void setup() {
+
+    }
+
+
+    // tests
+
+    public static void test1() {
+
+    }
+
+    public static void p(String s) {
+        System.out.println(p);
+    }
+
+}
